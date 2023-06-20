143994e8ab6ecf1671850d63d046fe76560e1528
Nelson Bravo WTW <nbravo@wtw.cl>
2023-06-20 16:23:03 -0400

    make 'undo' available| create matrix of block and make the origin to the base of the each block

--- samuray.py
+++ samuray.py
@@ -9,7 +9,7 @@ print(f"------->{dir}")
 class funcs():
     
     def change_origin ():
-        
+        print(f"-------change_origin>{dir}")
         # store the location of current 3d cursor
         saved_location = bpy.context.scene.cursor.location.xyz   # returns a vector
         
@@ -78,17 +78,108 @@ class funcs():
         # set 3dcursor location back to the stored location
         bpy.context.scene.cursor.location = saved_location
 
+    def create_block_greed ():
+        print(f"-------create_block_greed>{dir}")
+        # IN METERS
+        
+        foam_size_X = 1.410
+        foam_size_Y = 0.980
+        foam_size_Z = 1.180/2
+        scale = 1
+                                          
+        # create primitive cube as foamBlock
+        # change cursor location
+        bpy.context.scene.cursor.location =(0,0,0)
+        bpy.ops.mesh.primitive_cube_add(size=scale)
+        foamBlock = bpy.context.object        
+        foamBlock.name = "foamBlock.001"
+        foamBlock.dimensions = (foam_size_X, foam_size_Y, foam_size_Z)
+        bpy.ops.object.transform_apply(scale=True)
+
+        # Obtener el objeto al que se le aplicará el modificador
+        obj = bpy.context.object
+        # Move the object
+        obj.location = (0,0,foam_size_Z/2)
+
+        # change cursor location
+        bpy.context.scene.cursor.location =(0,0,0)
+        # set the origin on the current object to the 3dcursor location
+        bpy.ops.object.origin_set(type='ORIGIN_CURSOR')
+
+        # Agregar el modificador Array
+        array_modifier1 = obj.modifiers.new(name="Array", type='ARRAY')
+        array_modifier2 = obj.modifiers.new(name="Array", type='ARRAY')
+
+        # Definir las propiedades del modificador
+        array_modifier1.count = 4  # Número de repeticiones
+        array_modifier1.relative_offset_displace = (0.0, 1.0, 0.0)  # Desplazamiento relativo
+        array_modifier1.use_constant_offset = True  # Usar desplazamiento constante
+        array_modifier1.constant_offset_displace = (0.0, 0.02*scale, 0.0)  # Desplazamiento constante
+
+        # Definir las propiedades del modificador
+        array_modifier2.count = 5  # Número de repeticiones
+        array_modifier2.relative_offset_displace = (1.0, 0.0, 0.0)  # Desplazamiento relativo
+        array_modifier2.use_constant_offset = True  # Usar desplazamiento constante
+        array_modifier2.constant_offset_displace = (0.02*scale, 0.0, 0.0)  # Desplazamiento constante
+
+        # Aplicar el modificador
+        bpy.ops.object.modifier_apply(modifier=array_modifier1.name)
+        bpy.ops.object.modifier_apply(modifier=array_modifier2.name)
+
+        # Separar por partes sueltas
+        bpy.ops.mesh.separate(type='LOOSE')
+
+        # Obtener las partes separadas
+        foamBlocks = bpy.context.selected_objects
+
+        # Crear una nueva colección llamada "Blocks"
+        blocks_collection = bpy.data.collections.new("Blocks")
+
+        # Agregar las partes a la colección "Blocks"
+        for block in foamBlocks:
+            # set the origin on the current object to the ceter of mass location
+            bpy.ops.object.origin_set(type='ORIGIN_CENTER_OF_MASS')
+            # get object coordinates
+            coordinates = block.location            
+            # get new origin coordinates
+            block_location_X = coordinates.x
+            block_location_Y = coordinates.y
+            block_location_Z = coordinates.z
+            # change cursor location
+            bpy.context.scene.cursor.location = (block_location_X,block_location_Y,0)
+            # set the origin on the current object to the 3dcursor location
+            bpy.ops.object.origin_set(type='ORIGIN_CURSOR')
+            original_collection = block.users_collection[0]
+            original_collection.objects.unlink(block)
+            blocks_collection.objects.link(block)
+        # Agregar la colección "Blocks" a la escena
+        bpy.context.scene.collection.children.link(blocks_collection)
+
 # BUTTON CUSTOM (OPERATOR)
 ####################################################
-class BUTTOM_CUSTOM(bpy.types.Operator):
-    bl_label = "BUTTOM_CUSTOM"
-    bl_idname = "object.button_custom"
+class BUTTOM_CUSTOM01(bpy.types.Operator):
+    bl_label = "BUTTOM_CUSTOM01"
+    bl_idname = "object.button_custom01"
+    bl_options = {'UNDO'}
 
     def execute(self, context):
 
         funcs.change_origin()
         
-        print("execute button custom ok!")
+        print("execute button01 custom ok!")
+
+        return {'FINISHED'}
+    
+class BUTTOM_CUSTOM02(bpy.types.Operator):
+    bl_label = "BUTTOM_CUSTOM02"
+    bl_idname = "object.button_custom02"
+    bl_options = {'UNDO'}
+
+    def execute(self, context):
+
+        funcs.create_block_greed()
+        
+        print("execute button02 custom ok!")
 
         return {'FINISHED'}
     
@@ -107,23 +198,34 @@ class PANEL_CUSTOM_UI(bpy.types.Panel):
         layout = self.layout
 
         #create simple row
-        row = layout.row()
-        row.label(text = "Titulo Fila")
+        row01 = layout.row()
+        row01.label(text = "First Step")
 
         # add button custom
-        row = layout.row()
-        row.scale_y = 2
-        row.operator("object.button_custom", text= "Get set \n(Samuray)", icon = "HEART")
+        row01 = layout.row()
+        row01.scale_y = 2
+        row01.operator("object.button_custom01", text= "Prepare Work Area", icon = "MODIFIER_ON")
+
+        #create simple row
+        row02 = layout.row()
+        row02.label(text = "Second Step")
+
+        # add button custom
+        row02 = layout.row()
+        row02.scale_y = 2
+        row02.operator("object.button_custom02", text= "Make Cuts", icon = "MODIFIER_ON")
 
 # REGISTER (PART 2)
 ####################################################
 def register():
     bpy.utils.register_class(PANEL_CUSTOM_UI)
-    bpy.utils.register_class(BUTTOM_CUSTOM)
+    bpy.utils.register_class(BUTTOM_CUSTOM01)
+    bpy.utils.register_class(BUTTOM_CUSTOM02)
 
 def unregister():
     bpy.utils.unregister_class(PANEL_CUSTOM_UI)
-    bpy.utils.unregister_class(BUTTOM_CUSTOM)
+    bpy.utils.unregister_class(BUTTOM_CUSTOM01)
+    bpy.utils.unregister_class(BUTTOM_CUSTOM02)
 
 if __name__ == "__main__":
     register()
