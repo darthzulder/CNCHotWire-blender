f9037b280003d8454a8d6e60b054d72635f0aa50
Nelson Bravo WTW <nbravo@wtw.cl>
2023-06-22 14:22:18 -0400

    fix creation of planes and greedblocks related to the high of the object

--- samuray.py
+++ samuray.py
@@ -77,9 +77,12 @@ class funcs():
         # set 3dcursor location back to the stored location
         bpy.context.scene.cursor.location = saved_location
 
-    def create_block_greed (self,foam_size_X = 1.410, foam_size_Y = 0.980, foam_size_Z = 1.180/2, separation = 0.02, scale = 1):
+    def create_block_greed (self, dimensions_X, dimensions_Y, dimensions_Z, foam_size_X = 1.410, foam_size_Y = 0.980, foam_size_Z = 1.180/2, separation = 0.02, scale = 1):
         print(f"-------create_block_greed>{dir}")
         # IN METERS
+        n_blocks_x = math.ceil(dimensions_X/foam_size_X)
+        n_blocks_y = math.ceil(dimensions_Y/foam_size_Y)
+        n_blocks_z = math.ceil(dimensions_Z+separation/foam_size_Z)
                                           
         # create primitive cube as foamBlock
         # change cursor location
@@ -93,7 +96,7 @@ class funcs():
         # Obtener el objeto al que se le aplicará el modificador
         obj = bpy.context.object
         # Move the object
-        obj.location = (0,0,foam_size_Z/2)
+        obj.location = (separation/2,separation/2,foam_size_Z/2)
 
         # change cursor location
         bpy.context.scene.cursor.location =(0,0,0)
@@ -104,26 +107,35 @@ class funcs():
         obj.location = (foam_size_X/2,foam_size_Y/2,0)
         
         # Agregar el modificador Array
-        array_modifier1 = obj.modifiers.new(name="Array", type='ARRAY')
-        array_modifier2 = obj.modifiers.new(name="Array", type='ARRAY')
+        array_modifier1 = obj.modifiers.new(name="Array_Y", type='ARRAY')
+        array_modifier2 = obj.modifiers.new(name="Array_X", type='ARRAY')
 
         # Definir las propiedades del modificador
-        array_modifier1.count = 4  # Número de repeticiones
+        array_modifier1.count = n_blocks_y  # Número de repeticiones
         array_modifier1.relative_offset_displace = (0.0, 1.0, 0.0)  # Desplazamiento relativo
         array_modifier1.use_constant_offset = True  # Usar desplazamiento constante
         array_modifier1.constant_offset_displace = (0.0, separation*scale, 0.0)  # Desplazamiento constante
 
         # Definir las propiedades del modificador
-        array_modifier2.count = 5  # Número de repeticiones
+        array_modifier2.count = n_blocks_x  # Número de repeticiones
         array_modifier2.relative_offset_displace = (1.0, 0.0, 0.0)  # Desplazamiento relativo
         array_modifier2.use_constant_offset = True  # Usar desplazamiento constante
         array_modifier2.constant_offset_displace = (separation*scale, 0.0, 0.0)  # Desplazamiento constante
+
+        if n_blocks_z > 1 :
+            array_modifier3 = obj.modifiers.new(name="Array_Z", type='ARRAY')
+            array_modifier3.count = n_blocks_z  # Número de repeticiones
+            array_modifier3.relative_offset_displace = (0.0, 0.0, 1.0)  # Desplazamiento relativo
+            array_modifier3.use_constant_offset = True  # Usar desplazamiento constante
+            array_modifier3.constant_offset_displace = (0.0, 0.0, separation*scale)  # Desplazamiento constante
+            bpy.ops.object.modifier_apply(modifier=array_modifier3.name)
 
         # Aplicar el modificador
         bpy.ops.object.modifier_apply(modifier=array_modifier1.name)
         bpy.ops.object.modifier_apply(modifier=array_modifier2.name)
 
-        # Separar por partes sueltas
+        
+        # ------------------Separar por partes sueltas-------------------------
         bpy.ops.mesh.separate(type='LOOSE')
 
         # Obtener las partes separadas
@@ -137,13 +149,13 @@ class funcs():
             # set the origin on the current object to the ceter of mass location
             bpy.ops.object.origin_set(type='ORIGIN_CENTER_OF_MASS')
             # get object coordinates
-            coordinates = block.location            
+            coor_block = block.location            
             # get new origin coordinates
-            block_location_X = coordinates.x
-            block_location_Y = coordinates.y
-            block_location_Z = coordinates.z
+            block_location_X = coor_block.x
+            block_location_Y = coor_block.y
+            block_location_Z = coor_block.z
             # change cursor location
-            bpy.context.scene.cursor.location = (block_location_X,block_location_Y,0)
+            bpy.context.scene.cursor.location = (block_location_X,block_location_Y,block_location_Z-foam_size_Z/2)
             # set the origin on the current object to the 3dcursor location
             bpy.ops.object.origin_set(type='ORIGIN_CURSOR')
             original_collection = block.users_collection[0]
@@ -151,10 +163,11 @@ class funcs():
             blocks_collection.objects.link(block)
         # Agregar la colección "Blocks" a la escena
         bpy.context.scene.collection.children.link(blocks_collection)
-
-    def create_cutter_planes (self, dimensions_X,dimensions_Y, separation_x = 1.43, separation_y = 1,plane_thickness = 0.02, scale = 1):
         
-        plane_size_high = 1
+
+    def create_cutter_planes (self, dimensions_X,dimensions_Y, dimensions_Z, separation_x = 1.43, separation_y = 1, plane_thickness = 0.02, scale = 1):
+        
+        plane_size_high = (dimensions_Z+0.5)
 
         faces_count_x = math.ceil(dimensions_X/separation_x)+1 
         faces_count_y = math.ceil(dimensions_Y/separation_y)+1 
@@ -164,10 +177,11 @@ class funcs():
             
         location_x = (dim_plane_x-0.5)/2
         location_y = (dim_plane_y-0.5)/2
+        location_z = (plane_size_high-0.5)/2
         
         # -----create primitive Plane as cutterPlane --Cuts in X--
         # change cursor location
-        bpy.context.scene.cursor.location =(0,location_y,plane_size_high/2)
+        bpy.context.scene.cursor.location =(0,location_y,location_z)
         bpy.ops.mesh.primitive_plane_add(size=scale)
         cutterPlane1 = bpy.context.object
         cutterPlane1.name = "cutterPlane.001"
@@ -176,9 +190,9 @@ class funcs():
         bpy.ops.object.transform_apply(scale=True)
 
         # Agregar el modificador Array
-        array_modifier1 = cutterPlane1.modifiers.new(name="Array", type='ARRAY')
+        array_modifier1 = cutterPlane1.modifiers.new(name="Array_X", type='ARRAY')
         # Agregar el modificador Array
-        array_modifier2 = cutterPlane1.modifiers.new(name="Solidify", type='SOLIDIFY')
+        array_modifier2 = cutterPlane1.modifiers.new(name="Solidify_X", type='SOLIDIFY')
 
         # Definir las propiedades del modificador
         array_modifier1.count = faces_count_x  # Número de repeticiones
@@ -192,7 +206,7 @@ class funcs():
 
         # -----create primitive Plane as cutterPlane --Cuts in Y--
         # change cursor location
-        bpy.context.scene.cursor.location =(location_x,0,plane_size_high/2)
+        bpy.context.scene.cursor.location =(location_x,0,location_z)
         bpy.ops.mesh.primitive_plane_add(size=scale)
         cutterPlane2 = bpy.context.object
         cutterPlane2.name = "cutterPlane.002"
@@ -201,9 +215,9 @@ class funcs():
         bpy.ops.object.transform_apply(scale=True)
 
         # Agregar el modificador Array
-        array_modifier3 = cutterPlane2.modifiers.new(name="Array", type='ARRAY')
+        array_modifier3 = cutterPlane2.modifiers.new(name="Array_Y", type='ARRAY')
         # Agregar el modificador Array
-        array_modifier4 = cutterPlane2.modifiers.new(name="Solidify", type='SOLIDIFY')
+        array_modifier4 = cutterPlane2.modifiers.new(name="Solidify_Y", type='SOLIDIFY')
 
         # Definir las propiedades del modificador
         array_modifier3.count = faces_count_y  # Número de repeticiones
@@ -279,15 +293,15 @@ class funcs():
         dimensions_Y = dimensions.y
         dimensions_Z = dimensions.z   
 
-        self.create_cutter_planes(dimensions_X,dimensions_Y) 
+        self.create_cutter_planes(dimensions_X,dimensions_Y,dimensions_Z, scale = 1) 
         
-        #self.create_block_greed()
+        self.create_block_greed(dimensions_X,dimensions_Y,dimensions_Z, scale = 1)
        
+        bpy.ops.object.select_all(action='DESELECT')
+        selected_object.select_set(True)
+        bpy.context.view_layer.objects.active = selected_object
         # change cursor location
-        bpy.context.scene.cursor.location =(0,0,0)
-        # set the origin on the current object to the 3dcursor location
-        bpy.ops.object.origin_set(type='ORIGIN_CURSOR')
-              
+        bpy.context.scene.cursor.location =(0,0,0)              
         # set 3dcursor location back to the stored location
         bpy.context.scene.cursor.location = saved_location
         
